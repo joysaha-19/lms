2c8f69ab402c535dffa469524b9d32334a8f91c7
Joydeep Saha <joysaha1002.com>
2024-05-07 08:24:49 +0530

    adding a course

--- Frontend/src/newcourse/newcourse.js
+++ Frontend/src/newcourse/newcourse.js
@@ -1,36 +1,139 @@
-import React, { useState } from 'react';
+import React, { useState , useRef ,useEffect } from 'react';
 import './newcourse.css';
+import { useNavigate } from 'react-router-dom';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import DoneIcon from '@mui/icons-material/Done';
+import UploadIcon from '@mui/icons-material/Upload';
 
 const MainContainer = () => {
+  const nav=useNavigate(null);
   const [editing, setEditing] = useState(false);
-  const [addingChapter, setAddingChapter] = useState(false);
-  const [newChapterName, setNewChapterName] = useState('');
-  const [newChapterDescription, setNewChapterDescription] = useState('');
-  const [editingIndex, setEditingIndex] = useState(-1);
+    const [readOnly, setReadOnly] = useState(false);
+    const [addingChapter, setAddingChapter] = useState(false);
+    const [newChapterName, setNewChapterName] = useState('');
+    const [newChapterDescription, setNewChapterDescription] = useState('');
+    const [editingIndex, setEditingIndex] = useState(-1);
+    const [image, setImage] = useState(null);
+    const fileInputRef = useRef(null);
 
-  const [chapters, setChapters] = useState([
-    {
-      name: "Introduction to AI",
-      description: "Explore the fundamentals of Artificial Intelligence and its impact on technology and society."
-    },
-    {
-      name: "History of AI",
-      description: "Trace the development of AI from early concepts to modern algorithms and breakthroughs."
-    },
-    {
-      name: "AI and Machine Learning",
-      description: "Understand how machine learning algorithms form the backbone of AI and its practical applications."
-    },
-    {
-      name: "Ethics in AI",
-      description: "Discuss the ethical considerations, potential biases, and future implications of AI in decision-making processes."
+    const [courseTitle, setCourseTitle] = useState('');
+    const [courseDescription, setCourseDescription] = useState('');
+    const [courseCost, setCourseCost] = useState('');
+
+    const [editCourseTitle, setEditCourseTitle] = useState(false);
+    const [editCourseDescription, setEditCourseDescription] = useState(false);
+    const [editCourseCost, setEditCourseCost] = useState(false);
+    const [completedFields, setCompletedFields] = useState(0);
+    const [loading1,setLoading1]=useState(true);
+    const [teacherid, setteacherid] = useState();
+
+    const username="Joydeep";
+    async function fetchTeacher(username) {
+      const encodedUsername = encodeURIComponent(username);
+      const url = `http://localhost:5000/lms/teachers/getteacher?username=${encodedUsername}`;
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setteacherid(data["_id"]);
+       
+      } catch (error) {
+        console.error("Failed to fetch teacher data:", error);
+      } finally {
+      }
+      setLoading1(false);
     }
-  ]);
+
+
+
+  
+    useEffect(() => {
+      fetchTeacher(username);
+      
+  
+    }, []);
+    
+
+
+
+
+    const submitCourse = async () => {
+      if(completedFields<5)
+        {
+          alert("All fields are mandatory");
+        return;
+        }
+      const courseData = {
+          course_name: courseTitle,
+          course_desc: "",
+          course_instructor: username,
+          course_cost: Number(courseCost),
+          enrolled: [],
+          teacherid: teacherid,
+          chapters: chapters,
+          tag: courseDescription  // Assuming tag is static, change if needed
+      };
+
+      try {
+          const response = await fetch('http://localhost:5000/lms/courses/addcourse', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(courseData)
+          });
+          const responseData = await response.json();
+          if (response.ok) {
+              console.log('Course added successfully:', responseData);
+          } else {
+              console.log('Failed to add course:', responseData);
+          }
+      } catch (error) {
+          console.error('Failed to send course data:', error);
+      }
+  };
+
+
+
+
+    const [chapters, setChapters] = useState([]);
+
+    useEffect(() => {
+        const fieldsFilled = [
+            courseTitle !== '',
+            courseDescription !== '',
+            courseCost !== '',
+            image !== null,
+            chapters.length > 1
+        ];
+        setCompletedFields(fieldsFilled.filter(Boolean).length);
+    }, [courseTitle, courseDescription, courseCost, image, chapters]);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            setImage(imageUrl);
+        }
+    };
+
+    const handleClickIcon = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const handleEditClick = (setter) => {
+        setter(true);
+    };
+
+    const handleDoneClick = (setter) => {
+        setter(false);
+    };
+ 
 
   function handleAddOrEditChapter(action) {
     if (action === 'add') {
@@ -49,13 +152,43 @@ const MainContainer = () => {
     setNewChapterDescription('');
     setAddingChapter(false);
     setEditing(false);
+    setReadOnly(false);
     setEditingIndex(-1);
+  }
+
+  function handleEditChapter(index) {
+    setEditing(true);
+    setReadOnly(false);
+    setAddingChapter(true);
+    setEditingIndex(index);
+    setNewChapterName(chapters[index].name);
+    setNewChapterDescription(chapters[index].description);
+  }
+
+  function handleChapterInfoClick(index) {
+    setEditing(false);
+    setReadOnly(true);
+    setAddingChapter(true);
+    setNewChapterName(chapters[index].name);
+    setNewChapterDescription(chapters[index].description);
   }
 
   function handleDeleteChapter(index) {
     const updatedChapters = chapters.filter((_, idx) => idx !== index);
     setChapters(updatedChapters);
-  }
+
+    // Check if the deleted chapter is the one being viewed or edited
+    if (index === editingIndex||readOnly) {
+        // Reset these values to close the editing or read-only form
+        setAddingChapter(false);
+        setEditing(false);
+        setReadOnly(false);
+        setEditingIndex(-1);
+        setNewChapterName('');
+        setNewChapterDescription('');
+    }
+}
+
 
   function moveChapterUp(index) {
     if (index === 0) return;
@@ -71,86 +204,190 @@ const MainContainer = () => {
     setChapters(newChapters);
   }
 
-  function handleEditChapter(index) {
-    setEditing(true);
-    setAddingChapter(true);
-    setEditingIndex(index);
-    setNewChapterName(chapters[index].name);
-    setNewChapterDescription(chapters[index].description);
-  }
-
   function truncateText(text, maxLength) {
     return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
   }
 
+ const nochapters=<div className='no_chapters'>
+No chapters have been added
+ </div>
+
+ const add_chapters=<div className='add_chapter_box'>
+ <div className='add_chapter_area'>
+   <div className='add_chapters_title'>
+     <p>Add Chapters (min 2)</p>
+   </div>
+   <div className='chapter_controls_box'>
+     <div className='add_chapter_button' style={{ display: !addingChapter ? 'flex' : 'none' }} onClick={() => { setAddingChapter(true); setEditing(false); setReadOnly(false); }}>
+       <AddCircleOutlineIcon />&nbsp;<p>Add chapter</p>
+     </div>
+     <div className='cancel_button' style={{ display: addingChapter ? 'flex' : 'none' }} onClick={() => { setAddingChapter(false); setEditing(false); setReadOnly(false); setNewChapterName(''); setNewChapterDescription(''); }}><p>Cancel</p></div>
+     <div className='add_button' style={{ display: addingChapter && !readOnly ? 'flex' : 'none' }} onClick={() => handleAddOrEditChapter(editing ? 'edit' : 'add')}>
+       <p>{editing ? 'Save' : 'Add'}</p>
+     </div>
+
+     <div className='title_text_box' style={{ display: addingChapter ? 'flex' : 'none' }}>
+       <input
+         className='title_text'
+         type='text'
+         placeholder='E.g.: Introduction'
+         value={newChapterName}
+         onChange={e => setNewChapterName(e.target.value)}
+         readOnly={readOnly}
+         style={{ color: readOnly ? 'gray' : '' ,cursor:readOnly?'not-allowed':''}}
+       />
+       <div className='boxtag'>Chapter Title</div>
+     </div>
+     <div className='title_description_box' style={{ display: addingChapter ? 'flex' : 'none' }}>
+       <textarea
+         className='title_text'
+         type='text'
+         placeholder='In this chapter, we will...'
+         style={{ resize: 'none', color: readOnly ? 'gray' : '',cursor:readOnly?'not-allowed':'' }}
+         value={newChapterDescription}
+         onChange={e => setNewChapterDescription(e.target.value)}
+         readOnly={readOnly}
+       ></textarea>
+       <div className='boxtag'>Chapter Objective</div>
+     </div>
+   </div>
+ </div>
+
+ <div className='added_chapters_area'>
+   <div className='added_chapters_container' >
+     
+   {chapters.length ? chapters.map((chapter, index) => (
+     <div className='added_chapter_box' key={index}>
+       <div className='navigate_box'>
+         <div className='upbox' onClick={() => moveChapterUp(index)}>
+           <ArrowUpwardIcon />
+         </div>
+         <div className='downbox' onClick={() => moveChapterDown(index)}>
+           <ArrowDownwardIcon />
+         </div>
+       </div>
+       <div className='added_chapter_info' onClick={() => handleChapterInfoClick(index)}>
+         <div className='added_chapter_name'><p>{truncateText(chapter.name, 40)}</p></div>
+       </div>
+       <div className='edit_delete_box'>
+         <div className='editbox' onClick={() => handleEditChapter(index)}><EditIcon /></div>
+         <div className='deletebox' onClick={() => handleDeleteChapter(index)}><DeleteIcon /></div>
+       </div>
+     </div>
+   )) : <div className='no_chapters'>No chapters have been added</div>}
+   </div>
+ </div>
+</div>
+
+const inputStyle = (editable) => ({
+  cursor: editable ? 'text' : 'not-allowed',
+  color: editable ? 'black' : 'gray'
+});
+
+const handleSelectChange = (e) => {
+  setCourseDescription(e.target.value);
+};
+
+
+if (loading1 ) {
+  return <div className="loading">Loading course info...</div>;
+}
   return (
     <div className='newcourse_parent'>
-      <div className='add_chapter_box'>
-        <div className='add_chapter_area'>
-          <div className='add_chapters_title'>
-            <p>Add Chapters</p>
-          </div>
-          <div className='chapter_controls_box'>
-            <div className='add_chapter_button' style={{ display: !addingChapter ? 'flex' : 'none' }} onClick={() => { setAddingChapter(true); setEditing(false); }}>
-              <AddCircleOutlineIcon />&nbsp;<p>Add chapter</p>
+    <div className='leftpanel'>
+        <div className='newcourse_titlebox'>
+            <div className='newcourse_title_container'><p>New Course Setup</p>
+                <p style={{ fontSize: '14px' }}>Completed Fields:{completedFields}/5</p>
             </div>
-            <div className='cancel_button' style={{ display: addingChapter ? 'flex' : 'none' }} onClick={() => { setAddingChapter(false); setEditing(false); setNewChapterName(''); setNewChapterDescription(''); }}><p>Cancel</p></div>
-            <div className='add_button' style={{ display: addingChapter ? 'flex' : 'none' }} onClick={() => handleAddOrEditChapter(editing ? 'edit' : 'add')}>
-              <p>{editing ? 'Save' : 'Add'}</p>
-            </div>
-
-            <div className='title_text_box' style={{ display: addingChapter ? 'flex' : 'none' }}>
-              <input
-                className='title_text'
-                type='text'
-                placeholder='E.g.: Introduction'
-                value={newChapterName}
-                onChange={(e) => setNewChapterName(e.target.value)}
-              />
-              <div className='boxtag'>Chapter Title</div>
-            </div>
-            <div className='title_description_box' style={{ display: addingChapter ? 'flex' : 'none' }}>
-              <textarea
-                className='title_text'
-                type='text'
-                placeholder='In this chapter, we will...'
-                style={{ resize: 'none' }}
-                value={newChapterDescription}
-                onChange={(e) => setNewChapterDescription(e.target.value)}
-              ></textarea>
-              <div className='boxtag'>Chapter Objective</div>
-            </div>
-          </div>
         </div>
-
-        <div className='added_chapters_area'>
-          <div className='added_chapters_container'>
-            
-            {chapters.map((chapter, index) => {
-              return (
-                <div className='added_chapter_box' key={index}>
-                  <div className='navigate_box'>
-                    <div className='upbox' onClick={() => moveChapterUp(index)}>
-                      <ArrowUpwardIcon />
-                    </div>
-                    <div className='downbox' onClick={() => moveChapterDown(index)}>
-                      <ArrowDownwardIcon />
-                    </div>
-                  </div>
-                  <div className='added_chapter_info'>
-                    <div className='added_chapter_name'><p>{truncateText(chapter.name, 40)}</p></div>
-                  </div>
-                  <div className='edit_delete_box'>
-                    <div className='editbox' onClick={() => handleEditChapter(index)}><EditIcon /></div>
-                    <div className='deletebox' onClick={() => handleDeleteChapter(index)}><DeleteIcon /></div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+        <div className='newcourse_namebox'>
+            <div className='newcourse_namebox_title'><p>Course Title</p></div>
+            <div className='newcourse_name_textbox'>
+                <input
+                    className='course_name_text'
+                    type='text'
+                    value={courseTitle}
+                    onChange={e => setCourseTitle(e.target.value)}
+                    readOnly={!editCourseTitle}
+                    style={inputStyle(editCourseTitle)}
+                />
+                {editCourseTitle ? (
+                    <DoneIcon className='tickicon' onClick={() => handleDoneClick(setEditCourseTitle)} />
+                ) : (
+                    <EditIcon className='editicon' onClick={() => handleEditClick(setEditCourseTitle)} />
+                )}
+            </div>
         </div>
-      </div>
+        <div className='newcourse_descriptionbox'>
+            <div className='newcourse_namebox_title'><p>Course Tag</p></div>
+            <div className='newcourse_description_textbox'>
+            <select className='course_description_text' value={courseDescription} onChange={handleSelectChange} style={{ width: '90%', height: '35px' }}>
+              <option value="Engineering">Engineering</option>
+              <option value="Art">Art</option>
+              <option value="Medical">Medical</option>
+              <option value="Science">Science</option>
+              <option value="Humanities">Humanities</option>
+          </select>
+                
+            </div>
+        </div>
+        <div className='newcourse_imagebox'>
+            <div className='newcourse_namebox_title'><p>Course Thumbnail</p></div>
+            <div className='newcourse_image_area'>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    style={{ display: 'none' }}
+                    ref={fileInputRef}
+                />
+                {!image && (
+                    <div style={{ textAlign: 'center' }}>
+                        Upload thumbnail here
+                        <UploadIcon className='uploadicon' onClick={handleClickIcon} />
+                    </div>
+                )}
+                {image && (
+                    <>
+                        <img src={image} alt="Uploaded" style={{ width: '400px', height: '200px', objectFit: 'contain' }} />
+                        <UploadIcon className='tickicon' onClick={handleClickIcon} />
+                    </>
+                )}
+            </div>
+        </div>
     </div>
+    <div className='rightpanel'>
+      {/* <div className='exitbox' >
+        <div className='newcourse_tag'></div>
+        <div className='exitbutton' onClick={()=>nav('/teacher')} style={{position:'absolute',top:'30%',right:'5%'}}>EXIT</div>
+      </div> */}
+        <div className='newcourse_chapterbox'>
+            {add_chapters}
+        </div>
+        <div className='newcourse_cost'>
+            <div className='newcourse_namebox_title'><p>Course Cost in $</p></div>
+            <div className='newcourse_name_textbox'>
+                <input
+                    className='course_name_text'
+                    type='number'
+                    value={courseCost}
+                    onChange={e => setCourseCost(e.target.value)}
+                    readOnly={!editCourseCost}
+                    style={inputStyle(editCourseCost)}
+                />
+                {editCourseCost ? (
+                    <DoneIcon className='tickicon' onClick={() => handleDoneClick(setEditCourseCost)} />
+                ) : (
+                    <EditIcon className='editicon' onClick={() => handleEditClick(setEditCourseCost)} />
+                )}
+            </div>
+        </div>
+        <div className='submitoptions'>
+            <div className='publishoptionbutton'  onClick={submitCourse}>Publish</div>
+            <div className='saveoptionbutton' onClick={()=>nav('/teacher')}>Exit</div>
+        </div>
+    </div>
+</div>
   );
 };
 
