c1dcd2586a1a570e0e350dfa850995166dd5ce6d
Joydeep Saha <joysaha1002.com>
2024-05-20 21:02:17 +0530

    back host

--- Backend/server.js
+++ Backend/server.js
@@ -4,28 +4,26 @@ const connectDb = require("./config/dbconnection");
 const errorhandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
 
+const cors = require('cors');
 const port = 5000;
 connectDb();
+const corsOptions = {
+  origin: '*',  // Allow all origins
+  methods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],  // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization','Username','username','authorization']  // Custom headers allowed
+};
 
-// Enable express to parse JSON bodies
+// Apply CORS middleware with the defined options
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
-
-// Manually set CORS headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // or specify a specific domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization, Username, username, authorization');
-  next();
-});
-
 app.use("/lms/courses", require("./routes/courseroutes"));
 app.use("/lms/users", require("./routes/userroutes"));
 app.use("/lms/teachers", require("./routes/teacherroutes"));
 
-// Error handling middleware
 app.use(errorhandler);
 
-// Start the server
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
