49a30efac5b250c1c255e4c3289c15b494f76e4f
Joydeep Saha <joysaha1002.com>
2024-05-20 22:21:08 +0530

    back host

--- Backend/server.js
+++ Backend/server.js
@@ -7,11 +7,26 @@ const dotenv = require("dotenv").config();
 const cors = require('cors');
 const port = 5000;
 connectDb();
-const corsOptions = {
-  origin: 'http://localhost:3000',  // Allow all origins
-  methods: ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],  // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization','Username','username','authorization']  // Custom headers allowed
-};
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 // Apply CORS middleware with the defined options
 app.use(cors(corsOptions));
